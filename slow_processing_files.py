4131b2cbb140d74ed249f3ad26f338ae2ba8e560
GeoffreyDlvl <geoffrey.dlvl@gmail.com>
2021-08-09 15:55:32 +0200

    Upload archive file on local server

--- slow_processing_files.py
+++ slow_processing_files.py
@@ -1,8 +1,40 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, flash, jsonify, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return jsonify({'some_message': 'some_answer'})
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+ALLOWED_EXTENSIONS = set(['zip', 'rar'])
+
+def is_file_allowed(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_archive():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            resp = jsonify({'message' : 'No file part in the request'})
+            resp.status_code = 400
+            return resp      
+        
+    file = request.files['file']
+
+    if file.filename == '':
+        resp = jsonify({'message' : 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and is_file_allowed(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        resp = jsonify({'message' : 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message' : 'Allowed file types are rar and zip'})
+        resp.status_code = 400
+        return resp
