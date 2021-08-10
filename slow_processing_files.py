c7e0537dfe73b84c190ac09098206bbab2431b47
GeoffreyDlvl <geoffrey.dlvl@gmail.com>
2021-08-10 15:12:29 +0200

    Save state information inside ArchiveInfo class

--- slow_processing_files.py
+++ slow_processing_files.py
@@ -2,6 +2,26 @@ import os
 import time
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
+from enum import Enum
+
+class State(str, Enum):
+    NONE      = 'None'
+    UPLOADING = 'Uploading'
+    UPLOADED  = 'Uploaded'
+    CRACKING  = 'Cracking'
+    CRACKED   = 'Cracked'
+
+class ArchiveInfo:
+    state = State.NONE
+    is_cracked = False
+    password = None
+
+    def serialize(self):  
+        return {           
+            'state': self.state,
+            'is_cracked': self.cracked,
+            'password': self.password
+        }
 
 app = Flask(__name__)
 
@@ -11,6 +31,8 @@ if not os.path.isdir(UPLOAD_FOLDER):
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = set(['zip', 'rar'])
+
+archive_cracks = {}
 
 def is_file_allowed(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -31,8 +53,11 @@ def upload_archive():
         return resp
     if file and is_file_allowed(file.filename):
         filename = secure_filename(file.filename)
+        archive_cracks[filename] = ArchiveInfo()
+        archive_cracks[filename].state = State.UPLOADING
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        resp = jsonify({'message' : 'File successfully uploaded'})
+        archive_cracks[filename].state = State.UPLOADED
+        resp = jsonify({'message' : 'File successfully uploaded', 'filename': filename, 'archive_info': archive_cracks[filename].serialize()})
         resp.status_code = 201
         return resp
     else:
@@ -44,5 +69,7 @@ def upload_archive():
 def crack(filename):
     if filename not in os.listdir(app.config['UPLOAD_FOLDER']):
         return jsonify({'file': filename, 'message': 'File not found'})
-    time.sleep(5)
+    archive_cracks[filename].state = State.CRACKING
+    time.sleep(10)
+    archive_cracks[filename].state = State.CRACKED
     return jsonify({'file': filename, 'message': 'Crack successful!', 'password': 'the-password'})
